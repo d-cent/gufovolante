5a5bc52550d23ca47ad7581a1664b1a48c7c5532
Jaromil <jaromil@dyne.org>
2018-04-10 00:21:59 +0200

    deps updates

--- project.clj
+++ project.clj
@@ -5,18 +5,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies ^:replace [[org.clojure/clojure "1.8.0"]
                            [org.clojure/data.json "0.2.6"]
-                           [org.clojure/data.csv "0.1.3"]
+                           [org.clojure/data.csv "0.1.4"]
 
-                           [org.apache.commons/commons-compress "1.12"]
-                           [org.tukaani/xz "1.5"]
+                           [org.apache.commons/commons-compress "1.16.1"]
+                           [org.tukaani/xz "1.8"]
 
-                           [clj-http "3.1.0"]
-                           [cheshire "5.6.3"]
+                           [clj-http "3.8.0"]
+                           [cheshire "5.8.0"]
 
-                           [clojure-humanize "0.2.0"]
+                           [clojure-humanize "0.2.2"]
 
                            [clojure-csv/clojure-csv "2.0.2"]
-                           [semantic-csv "0.1.0"]
+                           [semantic-csv "0.2.0"]
 
                            ;; [gorilla-repl "0.3.6"]
                            ;; [incanter-gorilla "0.1.0"]
@@ -24,17 +24,17 @@
                            [huri "0.5.0-SNAPSHOT"]
 
                            ;; gorilla-repl deps
-                           [http-kit "2.1.19"]
+                           [http-kit "2.2.0"]
                            [ring/ring-json "0.4.0"]
-                           [compojure "1.5.1"]
-                           [org.slf4j/slf4j-api "1.7.21"]
-                           [ch.qos.logback/logback-classic "1.1.7"]
+                           [compojure "1.6.0"]
+                           [org.slf4j/slf4j-api "1.7.25"]
+                           [ch.qos.logback/logback-classic "1.2.3"]
                            [gorilla-renderable "2.0.0"]
                            [gorilla-plot "0.1.4"]
                            [javax.servlet/servlet-api "2.5"]
                            [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
-                           [cider/cider-nrepl "0.12.0"]
-                           [org.clojure/tools.nrepl "0.2.12"]
+                           [cider/cider-nrepl "0.16.0"]
+                           [org.clojure/tools.nrepl "0.2.13"]
                            ]
   :source-paths ["src"]
   :resource-paths ["resources"]
